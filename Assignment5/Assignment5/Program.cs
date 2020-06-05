665e7a8e7645d5d24b07406fd94e9ff3cdfd24c2
g18640383061 <g18640383061@gmail.com>
2020-06-04 18:53:57 -0700

    winMain

--- Assignment5/Assignment5/Program.cs
+++ Assignment5/Assignment5/Program.cs
@@ -20,10 +20,19 @@ namespace Assignment5
 
             Console.WriteLine("The game has ended with {0} with {1} health", myCharacter.Name, myCharacter.HealthPoints);
 
+            Inventory items = new Inventory(5);
+            Item item1 = new Item("judy", 3, ItemType.consumable);
+            Item item2 = new Item("Andy", 4, ItemType.equipment);
+            Item item3 = new Item("Lawrence", 5, ItemType.key);
+            items.Add(item1);
+            items.Add(item2);
+            items.Add(item3);
             // TODO: initialize the inventory
             // Add a few items
             // Remove a few items
             // show the inventory.
+            items.Remove(item1);
+            items.ShowInventory();
 
         }
     }
